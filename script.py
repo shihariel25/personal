7d029a490047b81506494184214ab8f866b129b0
shihariel25 <shihariel25@gmail.com>
2024-11-12 14:39:34 -0800

    Update script.py

--- script.py
+++ script.py
@@ -5,4 +5,4 @@ vendor = pd.read_csv(r"C:\Users\Documents\Sample\Vendor.csv")
 
 vendor[["group-id", "Vendor_Name"]] = group_similar_strings(vendor["VendorName"], min_similarity=0.57)
 
-vendor.to_csv(r"C:\Users\v-arielshih\Documents\Sample\Vendor_New.csv")
+vendor.to_csv(r"C:\Users\Documents\Sample\Vendor_New.csv")
