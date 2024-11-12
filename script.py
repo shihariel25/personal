e2aa1aa25c606fa8b7745bd74c909b557e174da2
shihariel25 <shihariel25@gmail.com>
2024-11-12 14:38:36 -0800

    Add files via upload
    Provincial Health spending with dummy dataset

---
+++ script.py
@@ -0,0 +1,8 @@
+import pandas as pd
+from string-grouper import group_string_similar
+
+vendor = pd.read_csv(r"C:\Users\v-arielshih\Documents\Sample\Vendor.csv")
+
+vendor[["group-id", "Vendor_Name"]] = group_similar_strings(vendor["VendorName"], min_similarity=0.57)
+
+vendor.to_csv(r"C:\Users\v-arielshih\Documents\Sample\Vendor_New.csv")
