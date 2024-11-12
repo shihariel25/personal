19efcae7c1b3f240b7e087aba3981014f1c9731c
shihariel25 <shihariel25@gmail.com>
2024-11-12 14:39:18 -0800

    Update script.py

--- script.py
+++ script.py
@@ -1,7 +1,7 @@
 import pandas as pd
 from string-grouper import group_string_similar
 
-vendor = pd.read_csv(r"C:\Users\v-arielshih\Documents\Sample\Vendor.csv")
+vendor = pd.read_csv(r"C:\Users\Documents\Sample\Vendor.csv")
 
 vendor[["group-id", "Vendor_Name"]] = group_similar_strings(vendor["VendorName"], min_similarity=0.57)
 
